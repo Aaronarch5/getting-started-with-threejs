275d378e37f21af69c975245a869f056ec994c0e
Aaron Archundia <63936853+Aaronarch5@users.noreply.github.com>
2024-07-29 23:44:36 -0600

    ControlsUpdate

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
-import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js';
-
+import { OrbitControls } from 'OrbitControls';
 const w = window.innerWidth;
 const h = window.innerHeight;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
